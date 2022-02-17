deb354dbed0728246a25a5a99fd1c16a408e2cd9
Rohit Katlaa <42460632+rohitkatlaa@users.noreply.github.com>
2022-02-17 10:18:11 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -4,7 +4,6 @@ print("Hello World Demo...08 Feb 2022 \n")
 print("Hello World Demo...08 Feb 2022 \n")
 print("Hello World Demo...08 Feb 2022 \n")
 print("Hello World Demo...08 Feb 2022 \n")
-print("Hello World Demo...08 Feb 2022 \n")
 
 
 
